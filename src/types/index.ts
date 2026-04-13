f8d27e57170721f59e86426512fa71d8470e04a6
Chen Yucan <yucan.chen@stonybrook.edu>
2026-04-13 19:40:03 -0400

    Player Valuation: added weight category and pitcher handling, step 6

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,43 +1,90 @@
 // ========== Player Data types ==========
 export interface HitterStats {
-    ab: number;
-    r: number;
-    h: number;
-    "1b": number;
-    "2b": number;
-    "3b": number;
-    hr: number;
-    rbi: number;
-    bb: number;
-    k: number;
-    sb: number;
-    cs: number;
-    avg: number;
-    obp: number;
-    slg: number;
-    fpts: number;
+    ab: number;   // at-bats
+    r: number;    // runs scored
+    h: number;    // hits
+    "1b": number; // singles
+    "2b": number; // doubles
+    "3b": number; // triples
+    hr: number;   // home runs
+    rbi: number;  // runs batted in
+    bb: number;   // walks
+    k: number;    // strikeouts
+    sb: number;   // stolen bases
+    cs: number;   // caught stealing
+    avg: number;  // batting average
+    obp: number;  // on-base percentage
+    slg: number;  // slugging percentage
+    fpts: number; // fantasy points
+}
+export interface PitcherStats {
+    w: number;    // wins
+    l: number;    // losses
+    era: number;  // earned run average
+    g: number;    // games pitched
+    gs: number;   // games started
+    cg: number;   // complete games
+    sho: number;  // shutouts
+    sv: number;   // saves
+    svo: number;  // save opportunities
+    ip: number;   // innings pitched
+    h: number;    // hits allowed
+    r: number;    // runs allowed
+    er: number;   // earned runs allowed
+    hr: number;   // home runs allowed
+    hb: number;   // hit batters
+    bb: number;   // walks allowed
+    so: number;   // strikeouts
+    whip: number; // walks + hits per inning pitched
+    avg: number;  // opponent batting average
 }
 
-// NOTE: Only the stats our algorithm calculates, not all categories are weighted
-/**
- * const categories: HitterCategory[] = [
-    "ab", "r", "h", "1b", "2b", "3b",
-    "hr", "rbi", "bb", "k", "sb", "cs",
-    "avg", "obp", "slg", "fpts"
-];
- */
-export type HitterCategory = keyof HitterStats;
+// Subset categories used for Player valuation
+export const HITTER_SCORING_CATEGORIES = [
+    "r",
+    "1b",
+    "2b",
+    "3b",
+    "hr",
+    "rbi",
+    "bb",
+    "k", // -rev-
+    "sb",
+    "cs", // -rev-
+    "obp",
+    "slg"
+] as const;
+export type HitterScoringCategory = typeof HITTER_SCORING_CATEGORIES[number];
+
+
+export const PITCHER_SCORING_CATEGORIES = [
+    "w",
+    "sv",
+    "so",
+    "ip",
+    "era", // -rev-
+    "whip", // -rev-
+    "avg" // -rev-
+] as const;
+export type PitcherScoringCategory = typeof PITCHER_SCORING_CATEGORIES[number];
+    
 // For Category Factor, how important each category is
-export type HitterCategoryWeights = Record<HitterCategory, number>;
+export type HitterCategoryWeights = Record<HitterScoringCategory, number>;
+export type PitcherCategoryWeights = Record<PitcherScoringCategory, number>;
+
 // Used for mean, sd, z-score
-export type HitterCategorySummary = Record<HitterCategory, number>;
+export type HitterCategorySummary = Record<HitterScoringCategory, number>;
+export type PitcherCategorySummary = Record<PitcherScoringCategory, number>;
+
 // {PlayerID, SetofData}
 export type PlayerHitterCategorySummaries = Record<string, HitterCategorySummary>;
+export type PlayerPitcherCategorySummaries = Record<string, PitcherCategorySummary>;
 
 
 export interface SeasonStats {
     seasons: number[];
     hitter: HitterStats;
+    pitcher: PitcherStats;
 }
 
 export interface Player {
@@ -58,9 +105,10 @@ export interface LeagueSettings {
     budget: number;
     rosterSize: number;
     teamCount: number;
-    // Partial: keys may be optional to include
-    // Record<keyof HitterStats, number>: object where every stat field map to a number
-    categoryWeights: Partial<Record<keyof HitterStats, number>>;
+    categoryWeights?: {
+        hitters?: Partial<HitterCategoryWeights>;
+        pitchers?: Partial<PitcherCategoryWeights>;
+    };
 }
 
 /** 
