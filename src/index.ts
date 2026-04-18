41951b341979d0c0125bd27134d698132822f6fd
ShaiC <Crespo.Shai@gmail.com>
2026-04-18 17:32:37 -0400

    Update README to include detailed information on endpoints and usage instructions Small Edit to fix fallback port value in index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { mockValuationRequest } from "@/__tests__/fixtures/valuationRequest";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 5000;
 const PUBLIC_DIR = path.join(__dirname, '..', 'public');
 
 // Path to player.json: __dirname is inside dist, we need to go up 1 level using ..
